b18296c4d80573181fb816aa0d4d0430e00c441a
thoattuc <thoattuc.hd@gmail.com>
2022-12-20 15:42:34 +0700

    init project

---
+++ hello_world/server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = '127.0.0.1'; //localhost
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World!\n');
+}); //render
+
+server.listen(PORT, hostname, () => {
+    console.log(`Server running at http://${hostname}:${PORT}/`);
+});
